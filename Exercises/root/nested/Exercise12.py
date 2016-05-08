59f087353174902f390b2fbb19f05aaac4e4240b
Tohmas <Tohmas@ASUS>
2016-05-08 13:32:44 +0200

    Exception for lists shorter than 2

--- Exercises/root/nested/Exercise12.py
+++ Exercises/root/nested/Exercise12.py
@@ -3,9 +3,15 @@ Created on 8 maj 2016
 
 @author: Tohmas
 '''
-a = [5, 10, 15, 20, 25]
 
 def first_and_last (l):
+    x = len(l)
+    if x < 2:
+        raise ValueError('first_and_last: list must have at least two elements')
     return l[0::len(l)-1]
 
+a = [5, 10, 15, 20, 25]
+print(first_and_last(a))
+
+a = [5]
 print(first_and_last(a))
