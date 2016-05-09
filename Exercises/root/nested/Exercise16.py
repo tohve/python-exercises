f24de9b0e21d78b0ed4425d1ef5541c13f174d7f
Tohmas <Tohmas@ASUS>
2016-05-09 23:23:41 +0200

    smarter

--- Exercises/root/nested/Exercise16.py
+++ Exercises/root/nested/Exercise16.py
@@ -3,34 +3,10 @@ Created on 9 maj 2016
 
 @author: Tohmas
 '''
-words = list("My Pass Better Come Back From Analog Time".split())
-from random import randint
+import string
+import random
 
-def main():
-    
-    while True:
-        s = input("Select strength (w)eak, (m)edium, (s)trong or (q)uit: ")
-        
-        if s == "q":
-            break
-        
-        p = []
-        if s == "w":
-            print("A lousy password:")
-            p = [words[randint(0,7)], words[randint(0,7)]]
-        elif s == "m":
-            print("Here comes a rather good password:")
-            while len(p) < 6:
-                p.append(chr(randint(97,122)))
-        elif s == "s":
-            print("Here comes a good strong password:")
-            while len(p) < 12:
-                p.append(chr(randint(33,126)))
-        else:
-            print("Erroneous selection, try again!")
-            continue
-        
-        print(''.join(p))
-    
-main()
-    
+def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+print(pw_gen(int(input('How many characters in your password?'))))
